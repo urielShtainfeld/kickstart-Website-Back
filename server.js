5407aedfc6aa0b9035eb455df469fda1a88f3acb
ushtinfeld <uriel.shtinfeld@gmail.com>
2018-08-01 23:25:30 +0300

    initial commit

--- server.js
+++ server.js
@@ -11,54 +11,146 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 mongoose.Promise = Promise
 
-var dbUrl = 'mongodb://kickstart:kick123@ds247101.mlab.com:47101/uriel_kickstart'
+var dbUrl = 'mongodb://kickstartuser:kick123@ds247101.mlab.com:47101/uriel_kickstart'
+var bcrypt = require('bcrypt');
 
-var Message = mongoose.model('Message', {
-    name: String,
-    message: String
+var UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    usertype: {
+        type: String,
+        required: true,
+    }
+});
+
+var ProjectSchema =  {
+    name: {
+        type: String,
+        unique: true
+    },
+    description: String,
+    imagePath: String,
+    daysLeft: String,
+    hoursLeft: String,
+    neededMoney: String,
+    moneyCollected: String,
+    linkToExample: String,
+    status: String,
+    owner: String
+}
+var User = mongoose.model('User', UserSchema);
+var Project =mongoose.model('Project',ProjectSchema);
+//authenticate input against database
+UserSchema.statics.authenticate = function (userName, password, callback) {
+    User.findOne({username: userName})
+        .exec(function (err, user) {
+            if (err) {
+                return callback(err)
+            } else if (!user) {
+                var err = new Error('User not found.');
+                err.status = 401;
+                return callback(err);
+            }
+            bcrypt.compare(password, user.password, function (err, result) {
+                if (result === true) {
+                    return callback(null, user);
+                } else {
+                    return callback();
+                }
+            })
+        });
+}
+
+//hashing a password before saving it to the database
+UserSchema.pre('save', function (next) {
+    var user = this;
+    bcrypt.hash(user.password, 10, function (err, hash){
+        if (err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    })
+});
+
+app.get('/project', (req, res) => {
+    Project.find({}, (err, projects) => {
+		console.log(projects);
+        res.send(projects)
+    })
+
 })
 
-app.get('/messages', (req, res) => {
-    Message.find({}, (err, messages) => {
-        res.send(messages)
+app.post('/signIn',async (req, res) => {
+    User.authenticate(req.body.username, req.body.password, function (error, user) {
+        if (error || !user) {
+            var err = new Error('Wrong email or password.');
+            err.message = 'Wrong email or password.';
+            console.log('Wrong email or password');
+            err.status = 401;
+            return next(err);
+        } else {
+            console.log('user SignIn called');
+            return res.json({usertype: user.usertype});
+        }
     })
 })
 
-app.post('/messages', async (req, res) => {
+app.post('/user', async (req, res) => {
+    if (req.body.username &&
+        req.body.password &&
+        req.body.usertype) {
+        var userData = {
+            username: req.body.username,
+            password: req.body.password,
+            usertype: req.body.usertype,
+        }
 
+        User.create(userData, function (err, user) {
+            if (err) {
+                return next(err)
+            }
+        });
+        console.log('user post called');
+        res.sendStatus(200)
+
+    }
+})
+
+app.post('/project', async (req, res) => {
     try {
-        var message = new Message(req.body)
+        var project = new Project(req.body)
 
-        var savedMessage = await message.save()
+        var savedProject = await project.save()
 
-        console.log('saved')
-
-        var censored = await Message.findOne({ message: 'badword' })
-
-        if (censored)
-            await Message.remove({ _id: censored.id })
-        else
-            io.emit('message', req.body)
+        io.emit('project', req.body)
 
         res.sendStatus(200)
     } catch (error) {
         res.sendStatus(500)
         return console.error(error)
     } finally {
-        console.log('message post called')
+        console.log('project post called')
     }
 })
 
 
+    io.on('connection', (socket) => {
+        console.log('a user connected')
+    })
 
-io.on('connection', (socket) => {
-    console.log('a user connected')
-})
+    mongoose.connect(dbUrl, {useMongoClient: true}, (err) => {
+        console.log('mongo db connection', err)
+    })
 
-mongoose.connect(dbUrl, { useMongoClient: true }, (err) => {
-    console.log('mongo db connection', err)
-})
-
-var server = http.listen(3000, () => {
-    console.log('server is listening on port', server.address().port)
-})
+    var server = http.listen(3000, () => {
+        console.log('server is listening on port', server.address().port)
+    })
